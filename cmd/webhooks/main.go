b12456e156f8478b341bf1c5da04cc21846a71d3
Alexander Greene <greene.al1991@gmail.com>
2023-10-31 15:13:05 -0700

    UPSTREAM: 754: Disable HTTP/2 for Webhook Servers
    Signed-off-by: Alexander Greene <greene.al1991@gmail.com>

--- cmd/webhooks/main.go
+++ cmd/webhooks/main.go
@@ -17,6 +17,7 @@ limitations under the License.
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"os"
@@ -30,6 +31,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	crwebhook "sigs.k8s.io/controller-runtime/pkg/webhook"
 
 	rukpakv1alpha1 "github.com/operator-framework/rukpak/api/v1alpha1"
 	"github.com/operator-framework/rukpak/internal/util"
@@ -53,13 +55,16 @@ func main() {
 	var probeAddr string
 	var systemNamespace string
 	var rukpakVersion bool
+	var enableHTTP2 bool
+
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.StringVar(&systemNamespace, "system-namespace", util.DefaultSystemNamespace, "Configures the namespace that gets used to deploy system resources.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
-	flag.BoolVar(&rukpakVersion, "version", false, "Displays rukpak version information")
+	flag.BoolVar(&enableHTTP2, "enable-http2", enableHTTP2, "If HTTP/2 should be enabled for the webhook servers.")
+
 	opts := zap.Options{
 		Development: true,
 	}
@@ -81,6 +86,19 @@ func main() {
 		os.Exit(1)
 	}
 	dependentSelector := labels.NewSelector().Add(*dependentRequirement)
+
+	// Setup webhook options
+	disableHTTP2 := func(c *tls.Config) {
+		if enableHTTP2 {
+			return
+		}
+		c.NextProtos = []string{"http/1.1"}
+	}
+
+	webhookServer := &crwebhook.Server{
+		TLSOpts: []func(config *tls.Config){disableHTTP2},
+	}
+
 	mgr, err := ctrl.NewManager(cfg, ctrl.Options{
 		Scheme:                 scheme,
 		MetricsBindAddress:     metricsAddr,
@@ -97,6 +115,7 @@ func main() {
 				Label: dependentSelector,
 			},
 		}),
+		WebhookServer: webhookServer,
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
